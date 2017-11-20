ae3ab2bf57b1829445a59e90302b694cb06c4dc7
Jocelyn1912 <toty_2410@hotmail.com>
2017-11-20 09:12:58 -0300

    comentarios maqueteo

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
+/*
+ * Funcionalidad de tu producto
+ */
 
+// Puedes hacer uso de la base de datos a través de la variable `data`
+console.log(data);
 
 /*Función de lista desplegable*/
 var btn = document.getElementById('btn1');
-var cont = document.getElementById('container');
+var cont = document.getElementById('uno');
 btn.addEventListener('click', function(){
 	var selection = document.getElementById('select').value;
 	if (selection == '0') {
 		alert('Elige una generación');
 		return false;
+	} cont.innerHTML = '<p> counter </p>';
+	
+});
+
+/*Extracción dato de alumnas activas en Arequipa generación 2 del 2016*/
+var students = data.SCL['2016-2'].students;
+for (var i = 0; i < students.length; i++) {
+	var counter = 0;
+	counter = counter + i;
 	}
-	cont.innerHTML = '<p> Hola, la generación seleccionada es: ' + selection + '</p>';
-})
+console.log(counter);
+
+	
+
