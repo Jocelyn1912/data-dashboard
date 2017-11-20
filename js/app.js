026117d537019daa2818f5567ada0841b5f59e9b
Jocelyn1912 <toty_2410@hotmail.com>
2017-11-20 10:10:05 -0300

    Función datos de alumnas inscritas, activas e inactivas

--- js/app.js
+++ js/app.js
@@ -13,17 +13,34 @@ btn.addEventListener('click', function(){
 	if (selection == '0') {
 		alert('Elige una generación');
 		return false;
-	} cont.innerHTML = '<p> counter </p>';
+	} cont.innerHTML = '<p> 30 </p>';
 	
 });
 
-/*Extracción dato de alumnas activas en Arequipa generación 2 del 2016*/
-var students = data.SCL['2016-2'].students;
+/*var students = data.SCL['2016-2'].students;
 for (var i = 0; i < students.length; i++) {
 	var counter = 0;
 	counter = counter + i;
 	}
 console.log(counter);
 
-	
+/*sacando datos de alumnas total, activas e inactivas*/
+var estudiantes = data.SCL['2016-2'].students;
+var inscritas = 0;
+var activas = 0;
+var bajas = 0;
 
+for (var i = 0; i < estudiantes.length; i++) {
+	inscritas++;
+	if (estudiantes[i].active == true) {
+		activas++;
+	} else if (estudiantes[i].active == false){
+		bajas++;
+	}	
+}
+
+console.log(activas);
+console.log(bajas);
+
+var porcentaje = parseInt((bajas * 100) / inscritas);
+console.log(porcentaje);
